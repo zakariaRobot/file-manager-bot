ac3f7656a2b49e1da1d06c93564f580e298125ff
zakariaRobot <rasoli.zakaria@gmail.com>
2016-04-13 06:44:50 -0700

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -9,14 +9,14 @@ local bot_api_key = ""
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 -- Base folder like
 -- local BASE_FOLDER = "/home/imandaneshi/files/"
-local BASE_FOLDER = ""
+local BASE_FOLDER = "198304093:AAGMH5V1oF38v_BX79NKjeQ3EvhQor12rjs"
 -------
 
 ----utilites----
 
 function is_admin(msg)-- Check if user is admin or not
   local var = false
-  local admins = {123456789,987654321}-- put your id here
+  local admins = {139328010}-- put your id here
   for k,v in pairs(admins) do
     if msg.from.id == v then
       var = true
